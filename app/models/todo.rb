b02e0ab3cd101aeec4067fdb4ba7b17d163bf078
Swapnil Jagtap <developedbysj@gmail.com>
2021-01-20 20:11:55 +0530

    Added ERB templates

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,20 @@
 class Todo < ActiveRecord::Base
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
   def to_pleasant_string
     "#{id}. #{due_date.to_s} #{todo_text} #{completed ? "[X]" : "[ ]"}"
   end
